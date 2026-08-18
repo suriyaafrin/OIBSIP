f960aa82df6e2b20dc487261281a9889f370c77d
suriyaafrin <suriyaislamafrin@gmail.com>
2026-08-18 10:53:39 +0600

    Add Tribute Page with HTML, CSS, and JavaScript, including layout, styling, and interactivity

---
+++ WebDev-L2-TributePage/script.js
@@ -0,0 +1,10 @@
+const revealEls = document.querySelectorAll('.reveal');
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting){
+        entry.target.classList.add('is-visible');
+        io.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.12 });
+  revealEls.forEach(el => io.observe(el));
